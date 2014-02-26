b8dcd2e18485d16b3c4a15d73db41b66925e3ed6
Vadim Graboys <dimva13@gmail.com>
2014-02-26 09:51:08 -0500

    add keyboard shortcuts to slideshow

--- frontend/app/scripts/controllers/slideshow.js
+++ frontend/app/scripts/controllers/slideshow.js
@@ -10,10 +10,27 @@ angular.module('frontendApp')
 
     $scope.activeSlide = 1;
 
+    // make sure activeSlide stays sane
+    $scope.$watch('activeSlide', function(val) {
+      val = (val - 0) || 1;
+      var numSlides = $scope.numSlides || 1;
+      $scope.activeSlide = Math.min(numSlides, Math.max(val, 1));
+    });
+
     $http.get('/num_slides/' + snapid).success(function(data) {
       $scope.numSlides = data.numSlides;
       $scope.fakeArray = new Array($scope.numSlides);
     }).error(function() {
       alert('Snapppt not found!');
+    });
+
+    // shortcuts
+    key('space, right', function() {
+      $scope.activeSlide++;
+      $scope.$digest();
+    });
+    key('left', function() {
+      $scope.activeSlide--;
+      $scope.$digest();
     });
   });
